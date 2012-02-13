7d90b3cca681e601572795c60a18e334934cd854
Jeremy Olmsted-Thompson <jeremyot@gmail.com>
2012-02-12 17:35:56 -0800

    updates for submission to pypi

--- setup.py
+++ setup.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+import os
 
+template_files = []
+for (path, dirs, files) in os.walk('templates'):
+  template_files.append((path, [os.path.join(path, f) for f in files]))
 setup(
   name='Toto',
   version='0.6',
   author='JeremyOT',
+  author_email='',
+  download_url='https://github.com/JeremyOT/Toto/zipball/master',
+  license='MIT License',
+  platforms=['OS X', 'Linux'],
   url='https://github.com/JeremyOT/Toto',
   packages=['toto',],
   requires=['tornado(>=2.1)',],
   provides=['toto',],
   scripts=['scripts/toto-create',],
-  data_files=[('templates/toto', ['templates',]),]
+  description='A Tornado based framework designed to accelerate web service development',
+  classifiers=['License :: OSI Approved :: MIT License', 'Operating System :: POSIX'],
+  data_files=template_files
   )
