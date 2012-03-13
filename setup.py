04b2a3180fd9a83e787e597faa8bdb062b567a65
Jeremy Olmsted-Thompson <jeremyot@gmail.com>
2012-03-13 00:21:53 -0700

    version bump

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ for (path, dirs, files) in os.walk('templates'):
 
 setup(
   name='Toto',
-  version='0.6.12',
+  version='0.7.0',
   author='JeremyOT',
   author_email='',
   download_url='https://github.com/JeremyOT/Toto/zipball/master',
