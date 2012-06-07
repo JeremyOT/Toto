0437780d195274dfd63509f04393ef043358af5b
Jeremy Olmsted-Thompson <jeremyot@gmail.com>
2012-06-07 07:45:55 -0700

    added zmq events

--- toto/deprecated_events.py
+++ toto/deprecated_events.py
@@ -1,43 +1,47 @@
 import cPickle as pickle
+from urllib2 import Request, urlopen
 from threading import Thread
 from collections import deque
 from tornado.web import *
 from tornado.ioloop import IOLoop
-from zqm.eventloop.ioloop import ZQMPoller
 from traceback import format_exc
 from tornado.options import options
-import zmq
 import logging
-import zlib
 
-_server_sockets = []
+_private_event_key = ''
+_server_routes = []
+_local_route = ''
 
-def add_socket(address):
-  _server_sockets.append(address)
+def _generate_key():
+  import os
+  from base64 import b64encode
+  return b64encode(os.urandom(128))
+
+def set_key(key):
+  global _private_event_key
+  _private_event_key = key or _generate_key()
+
+def set_local_route(route):
+  global _local_route
+  _local_route = route
+
+def add_route(route):
+  _server_routes.append(route)
+
+class EventHandler(RequestHandler):
+  
+  SUPPORTED_METHODS = ["POST",]
+  
+  def post(self):
+    if _private_event_key and not ('x-toto-event-key' in self.request.headers and self.request.headers['x-toto-event-key'] == _private_event_key):
+      raise HttpError(400, "Bad Request")
+    event = pickle.loads(self.request.body)
+    EventManager.instance().receive(event)
 
 class EventManager():
 
-  def __init__(self, address):
+  def __init__(self):
     self.__handlers = {}
-    self.address = address
-    self.__zmq_context = zmq.Context()
-    self.__socket = self.__zmq_context.socket(zmq.PULL)
-    self.__socket.bind(address)
-    self.__remote_servers = {}
-    self.__thread = None
-  
-  def register_server(self, address):
-    if address in self.__remote_servers:
-      raise Exception('Server already registered: %s', address)
-    socket = self.__zmq_context.socket(zmq.PUSH)
-    socket.connect(address)
-    self.__remote_servers[address] = socket
-
-  def remove_server(self, address):
-    del self.__remote_servers[address]
-    
-  def remove_all_servers(self):
-    self.__remote_servers.clear()
 
   def remove_handler(self, handler_sig):
     self.__handlers[handler_sig[0]].discard(handler_sig[1])
@@ -52,7 +56,7 @@ class EventManager():
   def receive(self, event):
     event_name = event['name']
     event_args = event['args']
-    if event_name in self.__handlers:
+    def event_thread():
       handlers = self.__handlers[event_name]
       persistent_handlers = set()
       while handlers:
@@ -66,15 +70,17 @@ class EventManager():
         if handler[3]:
           persistent_handlers.add(handler)
       handlers |= persistent_handlers
+    if event_name in self.__handlers:
+      Thread(target=event_thread).start()
   
-  def send_to_server(self, address, event_name, event_args):
+  def send_to_route(self, route, event_name, event_args):
     event = {'name': event_name, 'args': event_args}
-    event_data = zlib.compress(pickle.dumps(event))
-    self.__remote_servers[address].send(event_data)
+    event_data = pickle.dumps(event)
+    urlopen(Request(route, event_data, {'x-toto-event-key': _private_event_key}))    
   
-  def send(self, event_name, event_args, broadcast=False):
+  def send(self, event_name, event_args):
     event = {'name': event_name, 'args': event_args}
-    event_data = zlib.compress(pickle.dumps(event))
+    event_data = pickle.dumps(event)
     for route in _server_routes:
       if route == _local_route:
         self.receive(event)
@@ -84,16 +90,9 @@ class EventManager():
         except Exception as e:
           logging.error("Bad event route: %s - %s", route, options.debug and format_exc() or e)
 
-  def start(self):
-    def run():
-      while(True):
-        self.receive(pickle.loads(zlib.decompress(self.__socket.recv())))
-        
-    self.thread = threading.Thread()
-
   @staticmethod
   def instance():
     if not hasattr(EventManager, "_instance"):
-      EventManager._instance = EventManager(options.address)
+      EventManager._instance = EventManager()
     return EventManager._instance
       
