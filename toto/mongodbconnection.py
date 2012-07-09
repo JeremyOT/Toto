fb1dcec221eaa853656e2bbaa80253051e5a1deb
Jeremy Olmsted-Thompson <jeremyot@gmail.com>
2012-07-09 00:00:45 -0700

    mongodb connection now allows non-string user_id types

--- toto/mongodbconnection.py
+++ toto/mongodbconnection.py
@@ -57,10 +57,9 @@ class MongoDBConnection():
     self.anon_session_renew = anon_session_renew or self.anon_session_ttl
 
   def password_hash(self, user_id, password):
-    return hashlib.sha256(user_id + self.password_salt + password).hexdigest()
+    return hashlib.sha256(str(user_id).lower() + self.password_salt + password).hexdigest()
 
   def create_account(self, user_id, password, additional_values={}, **values):
-    user_id = user_id.lower()
     if self.db.accounts.find_one({'user_id': user_id}):
       raise TotoException(ERROR_USER_ID_EXISTS, "User ID already in use.")
     values.update(additional_values)
@@ -71,7 +70,6 @@ class MongoDBConnection():
   def create_session(self, user_id=None, password=None):
     if not user_id:
       user_id = ''
-    user_id = user_id.lower()
     account = user_id and password and self.db.accounts.find_one({'user_id': user_id, 'password': self.password_hash(user_id, password)})
     if user_id and not account:
       raise TotoException(ERROR_USER_NOT_FOUND, "Invalid user ID or password")
@@ -101,11 +99,9 @@ class MongoDBConnection():
     self.db.sessions.remove({'session_id': session_id})
 
   def clear_sessions(self, user_id):
-    user_id = user_id.lower()
     self.db.sessions.remove({'user_id': user_id})
 
   def change_password(self, user_id, password, new_password):
-    user_id = user_id.lower()
     account = self.db.accounts.find_one({'user_id': user_id, 'password': self.password_hash(user_id, password)})
     if not account:
       raise TotoException(ERROR_USER_NOT_FOUND, "Invalid user ID or password")
@@ -113,7 +109,6 @@ class MongoDBConnection():
     self.clear_sessions(user_id)
 
   def generate_password(self, user_id):
-    user_id = user_id.lower()
     account = self.db.accounts.find_one({'user_id': user_id})
     if not account:
       raise TotoException(ERROR_USER_NOT_FOUND, "Invalid user ID or password")
