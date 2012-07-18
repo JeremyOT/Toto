300e5f19af73e1fa546bea0471a4fc855e9fb83d
Jeremy Olmsted-Thompson <jeremyot@gmail.com>
2012-07-17 23:12:49 -0700

    bad variable name

--- toto/secret.py
+++ toto/secret.py
@@ -1,7 +1,7 @@
 from pbkdf2 import crypt
 
 def password_hash(secret):
-  return crypt(password)
+  return crypt(secret)
 
 def verify_password(secret, pwhash):
   return pwhash == crypt(secret, pwhash)
