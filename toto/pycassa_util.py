bb08402a199b0741206e09a3a1f9913979169018
Jeremy Olmsted-Thompson <jeremyot@gmail.com>
2012-07-23 21:50:50 -0700

    added batch-iteration to pycassa util

--- toto/pycassa_util.py
+++ toto/pycassa_util.py
@@ -1,31 +1,42 @@
 from pycassa import ColumnFamily
 
-def get_all(self, key, column_count=100, **kwargs):
+def get_all(self, key, column_count=100, yield_batch=False, **kwargs):
   kwargs['key'] = key
   kwargs['column_count'] = column_count
   results = self.get(**kwargs)
   result_count = len(results)
-  for k, v in results.iteritems():
-    yield k, v
+  if yield_batch:
+    k = next(reversed(results))
+    yield results
+  else:
+    for k, v in results.iteritems():
+      yield k, v
   while result_count == column_count:
     kwargs['column_start'] = k
     results = self.get(**kwargs)
     result_count = len(results)
     if result_count:
       results.popitem(False)
-    for k, v in results.iteritems():
-      yield k, v
+    if yield_batch:
+      k = next(reversed(results))
+      yield results
+    else:
+      for k, v in results.iteritems():
+        yield k, v
 
 ColumnFamily.get_all = get_all
 
-def get_columns(self, key, columns, column_count=100, **kwargs):
+def get_columns(self, key, columns, column_count=100, yield_batch=False, **kwargs):
   kwargs['key'] = key
   kwargs['column_count'] = column_count
   index = 0
   while index < len(columns):
     kwargs['columns'] = columns[index:index + column_count]
-    for k, v in self.get(**kwargs).iteritems():
-      yield k, v
+    if yield_batch:
+      yield self.get(**kwargs)
+    else:
+      for k, v in self.get(**kwargs).iteritems():
+        yield k, v
     index += column_count
 
 ColumnFamily.get_columns = get_columns
