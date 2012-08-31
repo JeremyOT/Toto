1e4bc05af432f6d4a428e1ac96ee5464f7ae6810
Jeremy Olmsted-Thompson <jeremyot@Jeremys-MacBook-Pro.local>
2012-08-30 23:29:40 -0700

    added get_page to pycassa util

--- toto/pycassa_util.py
+++ toto/pycassa_util.py
@@ -1,4 +1,5 @@
 from pycassa import ColumnFamily
+from itertools import islice
 
 def get_all(self, key, column_count=100, yield_batch=False, **kwargs):
   kwargs['key'] = key
@@ -40,3 +41,27 @@ def get_columns(self, key, columns, column_count=100, yield_batch=False, **kwarg
     index += column_count
 
 ColumnFamily.get_columns = get_columns
+
+def xmultiget(self, keys, buffer_size=0, *args, **kwargs):
+  buffer_size = buffer_size or self.buffer_size
+  key_iter = iter(keys)
+  key_batch = list(islice(key_iter, buffer_size))
+  while key_batch:
+    for k, v in self.multiget(key_batch, buffer_size=buffer_size, *args, **kwargs).iteritems():
+      yield k, v
+    key_batch = list(islice(key_iter, buffer_size))
+
+ColumnFamily.xmultiget = xmultiget
+
+def get_page(self, key, last_column=None, *args, **kwargs):
+  if last_column is None:
+    return self.get(key, *args, **kwargs)
+  else:
+    results = self.get(key, column_start=last_column, *args, **kwargs)
+    try:
+      del results[last_column]
+    except:
+      pass
+    return results
+
+ColumnFamily.get_page = get_page
