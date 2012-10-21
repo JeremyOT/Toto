1b064e7e11a8908a710130805cfcc9d25186d052
JeremyOT <jeremyot@msn.com>
2012-10-21 00:03:25 -0700

    added docs for tasks and exceptions

--- toto/exceptions.py
+++ toto/exceptions.py
@@ -1,3 +1,16 @@
+'''Toto uses the following error codes internally:
+
+  * ``ERROR_SERVER = 1000``
+  * ``ERROR_MISSING_METHOD = 1002``
+  * ``ERROR_MISSING_PARAMS = 1003``
+  * ``ERROR_NOT_AUTHORIZED = 1004``
+  * ``ERROR_USER_NOT_FOUND = 1005``
+  * ``ERROR_USER_ID_EXISTS = 1006``
+  * ``ERROR_INVALID_SESSION_ID = 1007``
+  * ``ERROR_INVALID_HMAC = 1008``
+  * ``ERROR_INVALID_RESPONSE_HMAC = 1009``
+'''
+
 ERROR_SERVER = 1000
 ERROR_MISSING_METHOD = 1002
 ERROR_MISSING_PARAMS = 1003
@@ -9,6 +22,11 @@ ERROR_INVALID_HMAC = 1008
 ERROR_INVALID_RESPONSE_HMAC = 1009
 
 class TotoException(Exception):
+  '''This class is used to return errors from Toto methods. ``TotoException.value``
+  is used to describe the exception and ``TotoException.code`` should be set to a
+  status code that can be used to programmatically reference the exception. Toto's
+  error redirecting capabilities use ``code`` to look up the redirect URL.
+  '''
   def __init__(self, code, value):
     self.value = value
     self.code = code
--- toto/tasks.py
+++ toto/tasks.py
@@ -1,3 +1,8 @@
+'''Toto provides build a built in task queue for performing work in the background
+while limiting the number of active jobs. The task queue is designed primarily for
+shorter, lightweight jobs. For CPU intensive tasks or tasks that are expected to
+run for a long time, look at Toto's worker functionality instead.
+'''
 from threading import Thread, Lock
 from collections import deque
 import logging
@@ -6,6 +11,9 @@ import traceback
 _task_queues = {}
 
 class TaskQueue():
+  '''Instances will run up to ``thread_count`` tasks at a time
+  whenever there are tasks in the queue.
+  '''
 
   def __init__(self, thread_count=1):
     self.tasks = deque()
@@ -14,13 +22,22 @@ class TaskQueue():
     self.threads = set()
     self.thread_count = thread_count
   
-  def add_task(self, fn, args=[]):
-    self.tasks.append((fn, args))
+  def add_task(self, fn, *args, **kwargs):
+    '''Add the function ``fn`` to the queue to be invoked with
+    ``args`` and ``kwargs`` as arguments. If the ``TaskQueue``
+    is not currently running, it will be started now.
+    '''
+    self.tasks.append((fn, args, kwargs))
     self.lock.acquire()
     self.run()
     self.lock.release()
 
   def run(self):
+    '''Start processing jobs in the queue. You should not need
+    to call this as ``add_task`` automatically starts the queue.
+    Processing threads will stop when there are no jobs available
+    in the queue.
+    '''
     if len(self.threads) >= self.thread_count:
       return
     thread = None
@@ -36,17 +53,24 @@ class TaskQueue():
           logging.error(traceback.format_exc())
         finally:
           self.lock.release()
-        task[0](*task[1])
+        task[0](*task[1], **task[2])
     thread = Thread(target=task_loop)
     thread.daemon = True
     self.threads.add(thread)
     thread.start()
 
   def __len__(self):
+    '''Returns the number of active threads plus the number of
+    queued tasks/'''
     return len(self.threads) + len(self.tasks)
 
   @staticmethod
   def instance(name, thread_count=1):
+    '''A convenience method for accessing shared instances of ``TaskQueue``.
+    If ``name`` references an existing instance created with this method,
+    that instance will be returned. Otherwise, a new ``TaskQueue`` will be
+    instantiated with ``thread_count`` threads and stored under ``name``.
+    '''
     try:
       return _task_queues[name]
     except KeyError:
