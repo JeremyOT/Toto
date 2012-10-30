7cab4308544791ced0fda4db08d683883bf9ce0c
JeremyOT <jeremyot@msn.com>
2012-10-29 23:35:33 -0700

    updated fakeconnection to match dbconnection

--- toto/fakeconnection.py
+++ toto/fakeconnection.py
@@ -1,8 +1,28 @@
-class FakeConnection():
+from dbconnection import DBConnection
+
+
+class FakeConnection(DBConnection):
 
   def __init__(self):
     self.db = None
 
-  def retrieve_session(self, *args):
+  def create_account(self, user_id, password, additional_values={}, **values):
+    pass
+
+  def create_session(self, user_id=None, password=None):
     return None
 
+  def retrieve_session(self, session_id, hmac_data=None, data=None):
+    return None
+
+  def remove_session(self, session_id):
+    pass
+
+  def clear_sessions(self, user_id):
+    pass
+
+  def change_password(self, user_id, password, new_password):
+    pass
+
+  def generate_password(self, user_id):
+    return None
