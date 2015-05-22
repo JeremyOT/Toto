acfa9e1855e1bbc3330c9dd11880483dfafbae7c
Jeremy Olmsted-Thompson <jeremyot@gmail.com>
2015-05-22 16:52:29 -0400

    task queue cleanup

--- toto/tasks.py
+++ toto/tasks.py
@@ -3,13 +3,14 @@ while limiting the number of active jobs. The task queue is designed primarily f
 shorter, lightweight jobs. For CPU intensive tasks or tasks that are expected to
 run for a long time, look at Toto's worker functionality instead.
 '''
-from threading import Thread, Lock
+from threading import Thread, Lock, Condition
 from collections import deque
 from tornado.gen import coroutine, Return
 from tornado.concurrent import Future
 from tornado.ioloop import IOLoop
-from Queue import Queue
+from Queue import Queue, Empty
 from sys import exc_info
+from itertools import count
 import logging
 import traceback
 
@@ -18,22 +19,24 @@ class TaskQueue():
   whenever there are tasks in the queue.
   '''
 
-  def __init__(self, thread_count=1):
+  def __init__(self, thread_count=1, idle_timeout=60, name='TaskQueue'):
     self.tasks = deque()
     self.running = False
-    self.lock = Lock()
+    self.condition = Condition()
     self.threads = set()
+    self.idle_timeout = idle_timeout
     self.thread_count = thread_count
+    self.name = name
 
   def add_task(self, fn, *args, **kwargs):
     '''Add the function ``fn`` to the queue to be invoked with
     ``args`` and ``kwargs`` as arguments. If the ``TaskQueue``
     is not currently running, it will be started now.
     '''
-    self.tasks.append((fn, args, kwargs))
-    self.lock.acquire()
-    self.run()
-    self.lock.release()
+    with self.condition:
+      self.tasks.append((fn, args, kwargs))
+      self.condition.notify()
+      self.run()
 
   def yield_task(self, fn, *args, **kwargs):
     '''Like add_task but will call the function as a coroutine, allowing you
@@ -76,28 +79,14 @@ class TaskQueue():
     '''Start processing jobs in the queue. You should not need
     to call this as ``add_task`` automatically starts the queue.
     Processing threads will stop when there are no jobs available
-    in the queue.
+    in the queue for at least ``idle_timeout`` seconds.
     '''
-    if len(self.threads) >= self.thread_count:
-      return
-    thread = None
-    def task_loop():
-      while 1:
-        self.lock.acquire()
-        try:
-          task = self.tasks.popleft()
-        except IndexError:
-          self.threads.remove(thread)
-          return
-        except Exception as e:
-          logging.error(traceback.format_exc())
-        finally:
-          self.lock.release()
-        task[0](*task[1], **task[2])
-    thread = Thread(target=task_loop)
-    thread.daemon = True
-    self.threads.add(thread)
-    thread.start()
+    with self.condition:
+      if len(self.threads) >= self.thread_count:
+        return
+      thread = self.__TaskLoop(self)
+      self.threads.add(thread)
+      thread.start()
 
   def __len__(self):
     '''Returns the number of active threads plus the number of
@@ -105,7 +94,7 @@ class TaskQueue():
     return len(self.threads) + len(self.tasks)
 
   @classmethod
-  def instance(cls, name, thread_count=1):
+  def instance(cls, name, thread_count=1, idle_timeout=60):
     '''A convenience method for accessing shared instances of ``TaskQueue``.
     If ``name`` references an existing instance created with this method,
     that instance will be returned. Otherwise, a new ``TaskQueue`` will be
@@ -116,8 +105,45 @@ class TaskQueue():
     try:
       return cls._task_queues[name]
     except KeyError:
-      cls._task_queues[name] = cls(thread_count)
+      cls._task_queues[name] = cls(thread_count, idle_timeout, name)
       return cls._task_queues[name]
+
+  class __TaskLoop(Thread):
+
+    thread_id = count()
+
+    def __init__(self, queue):
+      Thread.__init__(self, name='%s-%d' % (queue.name, self.thread_id.next()))
+      self.daemon = True
+      self.queue = queue
+      self.condition = self.queue.condition
+      self.tasks = self.queue.tasks
+      self.idle_timeout = self.queue.idle_timeout
+      self.threads = self.queue.threads
+      self.in_use = True
+
+    def run(self):
+      try:
+        while 1:
+          with self.condition:
+            if not len(self.tasks):
+              self.condition.wait(self.idle_timeout)
+            try:
+              task = self.tasks.popleft()
+            except IndexError:
+              logging.debug('Idle timeout: %s' % self.name)
+              self.threads.remove(self)
+              self.in_use = False
+              return
+          try:
+            task[0](*task[1], **task[2])
+          except Exception as e:
+            logging.error(traceback.format_exc())
+      finally:
+        with self.condition:
+          if self.in_use:
+            self.threads.remove(self)
+            self.in_use = False
 
 class InstancePool(object):
 
@@ -140,10 +166,27 @@ class InstancePool(object):
         self._pool.put(c)
     return call
 
-  def await(self, name=None):
-    if not name:
-      name = self._default_queue
-    return AwaitableInstance(self, name)
+  def await(self, queue_name=None):
+    if not queue_name:
+      queue_name = self._default_queue
+    return AwaitableInstance(self, queue_name)
+
+  def instance(self):
+    return self.__InstanceTransaction(self)
+
+  def transaction(self, function):
+    with self.instance() as i:
+      return function(i)
+
+  def async_transaction(self, function, queue_name=None):
+    if not queue_name:
+      queue_name = self._default_queue
+    TaskQueue.instance(queue_name).add_task(self.transaction, function)
+
+  def await_transaction(self, function, queue_name=None):
+    if not queue_name:
+      queue_name = self._default_queue
+    return TaskQueue.instance(queue_name).yield_task(self.transaction, function)
 
   def _get(self):
     return self._pool.get()
@@ -151,11 +194,23 @@ class InstancePool(object):
   def _put(self, i):
     return self._pool.put(i)
 
+  class __InstanceTransaction(object):
+
+    def __init__(self, pool):
+      self._pool = _pool
+
+    def __enter__(self):
+      self._instance = self._pool.get()
+      return self._instance
+
+    def __exit__(self):
+      self._pool.put(self._instance)
+
 class AwaitableInstance(object):
 
-  def __init__(self, instance, name="pool"):
+  def __init__(self, instance, queue_name="pool"):
     self.instance = instance
-    self.task_queue = TaskQueue.instance(name)
+    self.task_queue = TaskQueue.instance(queue_name)
 
   def __getattr__(self, key):
     return lambda *args, **kwargs: self.task_queue.yield_task(getattr(self.instance, key), *args, **kwargs)
